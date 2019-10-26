20b7ac8e2e5996ad5d5293016c2a6aa5c37a57cd
Sebastian Alvarez Sanchez <sebalvareztto@gmail.com>
2019-10-26 14:29:50 -0300

    Initial commit

---
+++ src/main/java/com/sitrack/soupcreator/SoupCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.sitrack.soupcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoupCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoupCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitrack/soupcreator/SoupCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitrack.soupcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoupCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
